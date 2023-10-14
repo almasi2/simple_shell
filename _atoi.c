f910344eb39c34e3b3a4cf23a8b5484c8713da64
Zulu-inventor33 <muteakelvin15@gmail.com>
2023-10-14 08:59:12 -0700

    atoi commit

---
+++ _atoi.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+
+/**
+ * _atoi - this will convert a string to an integer.
+ *
+ * @s: the pointer to str origin.
+ * Return: this will return 0.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int number = 0;
+	/*1- analisys sign*/
+	while (!('0' <= *s && *s <= '9') && *s != '\0')
+	{
+		if (*s == '-')
+			sign *= -1;
+		if (*s == '+')
+			sign *= +1;
+		s++;
+	}
+
+	/*2 - extract the number */
+	while ('0' <= *s && *s <= '9' && *s != '\0')
+	{
+
+		number = (number * 10) + (*s - '0');
+		s++;
+	}
+	return (number * sign);
+}
+
